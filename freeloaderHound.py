35545d0eb9c6db1e97a93bbb1018280ab0943035
zlatin <diglooclark@gmail.com>
2019-08-28 12:21:54 -0600

    v0.2
    added better doc, improved syntax and logic, broke tdelta logic into its own function

--- freeloaderHound.py
+++ freeloaderHound.py
@@ -1,45 +1,59 @@
-#leasegatekeeper.py
+#freeleasehound.py
 #2019 | dclark NCAR EOL/CWIG
 
 #monitors dhcp log for lingering devices on the network without a lease
 
-from datetime import datetime
+#progress: 3/7
+
+#done:
+#data intake, data parsing, regex,  
+#inprogress:
+#data logging and checking,
+#todo:
+#email, scheduling
+
+
+from datetime import datetime #don't ask me why this syntax enabled strptime and tdelta, I don't have an answer
 import os, re, sys
 
 print(sys.version)
 timeLimit = 3600 #seconds
 debug = False
-offendees = {}
+offenders = {}
+
+def timeDeltaCalc(time1, time2):
+	try:
+		tDelta = time2 - time1
+		return tDelta
+	except:
+		print("Time delta could not be calculated, time format must be wrong or missing")
 
 def sniffLease():
 	# loads, scans, and unloads log
 	# returns dictionary of offendees 
 
-	# Regex used to match relevant loglines
+	# Regex expressions:
 	regexMAC = re.compile(ur'([0-9a-f]{2}(?::[0-9a-f]{2}){5})')
 	regexFREE = "no free leases"
 	regexTIME = re.compile('(\w{3} \d{2} \d{2}:\d{2}:\d{2})')
 	timeFormat = "%b %d %H:%M:%S"
-	# Output file, where the matched loglines will be copied to
-	output_filename = os.path.normpath("tmp/dhcpfreeloaders.log")
-	# Overwrites the file, ensure we're starting out with a blank file
+	output_filename = os.path.normpath("tmp/dhcpfreeloaders.txt")
 
-
-	# Open input file in 'read' mode
+	#file IO:
 	with open('dhcpd.log', 'r') as inFile:
 		log = inFile.readlines()
-		# Loop over each log line
 		for line in log:
 			if regexFREE in line:
 				if(debug):
 					print(line)
-			# If log line matches our regex, print to console, and output file
-				MAC = regexMAC.findall(line)[0]
-				time = (datetime.strptime(regexTIME.findall(line)[0], timeFormat))
-				offendees [MAC] = [time, timeDelta
-		print offendees
+				MAC = regexMAC.findall(line)[0] # search for all mac addresses in line and set to MAC
+				time = (datetime.strptime(regexTIME.findall(line)[0], timeFormat)) #strip time and set to 'time'
+				timeDelta = timeDeltaCalc(time, time) #use tdelta calc to get time delta as timeDelta (if datetime function works, this will be removed)
+				offenders [MAC] = [time, timeDelta] #final collection of offenders, might change it to only save if tdelta is >3600
+		print offenders
 
 
 #def sendSniffs(snifflist)
+
 
 sniffLease()
