bf6500efbd511994f9551219c7e7e95f2dde6afa
zlatin <diglooclark@gmail.com>
2019-08-30 16:38:30 -0600

    v0.3
    Added:
    tDelta formatting and logic
    subnet regex & intake
    logic improvements
    writeOffenders function

--- freeloaderHound.py
+++ freeloaderHound.py
@@ -1,41 +1,47 @@
 #freeleasehound.py
 #2019 | dclark NCAR EOL/CWIG
-
-#monitors dhcp log for lingering devices on the network without a lease
-
-#progress: 3/7
+#https://github.com/dclarkco/freeloaderHound
+#monitors dhcp log for lingering devices on the network without a lease, and alerts sysadmins
 
 #done:
-#data intake, data parsing, regex,  
+#data intake, data parsing, regex, subnet 
+
 #inprogress:
-#data logging and checking,
+#data logging and checking, csv tmp storage, timeDelta
+
 #todo:
-#email, scheduling
+#email, scheduling, tmp directory config, testing
 
 
-from datetime import datetime #don't ask me why this syntax enabled strptime and tdelta, I don't have an answer
+from datetime import datetime 
 import os, re, sys
 
-print(sys.version)
+#print(sys.version)
 timeLimit = 3600 #seconds
-debug = False
+debug = 0
 offenders = {}
 
 def timeDeltaCalc(time1, time2):
+	timeFormat = "%b %d %H:%M:%S"
+
+
+	time1 = datetime.strptime(time1, timeFormat)
+	time2 = datetime.strptime(time2, timeFormat)
 	try:
-		tDelta = time2 - time1
+		tDelta = (time2 - time1)
 		return tDelta
 	except:
 		print("Time delta could not be calculated, time format must be wrong or missing")
 
 def sniffLease():
 	# loads, scans, and unloads log
-	# returns dictionary of offendees 
+	# returns dictionary of offenders 
 
 	# Regex expressions:
 	regexMAC = re.compile(ur'([0-9a-f]{2}(?::[0-9a-f]{2}){5})')
 	regexFREE = "no free leases"
 	regexTIME = re.compile('(\w{3} \d{2} \d{2}:\d{2}:\d{2})')
+	regexNET = re.compile('[/][0-9]{2}')
 	timeFormat = "%b %d %H:%M:%S"
 	output_filename = os.path.normpath("tmp/dhcpfreeloaders.txt")
 
@@ -44,16 +50,45 @@ def sniffLease():
 		log = inFile.readlines()
 		for line in log:
 			if regexFREE in line:
-				if(debug):
-					print(line)
+
 				MAC = regexMAC.findall(line)[0] # search for all mac addresses in line and set to MAC
-				time = (datetime.strptime(regexTIME.findall(line)[0], timeFormat)) #strip time and set to 'time'
-				timeDelta = timeDeltaCalc(time, time) #use tdelta calc to get time delta as timeDelta (if datetime function works, this will be removed)
-				offenders [MAC] = [time, timeDelta] #final collection of offenders, might change it to only save if tdelta is >3600
-		print offenders
+				NET = regexNET.findall(line)[0].strip('/')
+				TIME = regexTIME.findall(line)[0]
 
+				if MAC in offenders:
+					metadata = offenders[MAC]
+					tDelta = timeDeltaCalc(metadata[0], TIME)
+					
+					offenders[MAC].append(tDelta)
+					print(MAC + " is lingering on a free lease for " + str(tDelta) + "...")
 
-#def sendSniffs(snifflist)
+				# #strip time and set to 'time'
+				 #use tdelta calc and time parsing to get time delta as timeDelta 
+				offenders [MAC] = [TIME, NET] #final collection of offenders, change to only save if tdelta is >3600
+				if(debug):
+					print(line.strip('\n'))
+				#save offenders as csv, ensure that it doesn't grow beyond scope of the data being handled
+		if(debug):
+			print(offenders)
+		return offenders
+
+def writeOffenders(offenders):
+	import csv
+	with open('offenders.csv', 'w') as csv_file:
+		writer = csv.writer(csv_file)
+	for key, value in mydict.items():
+	   writer.writerow([key, value])
+
+	with open('dict.csv') as csv_file:
+		reader = csv.reader(csv_file)
+		mydict = dict(reader)
+
+def sendSniffs(snifflist):
+	#sends an email of the offenders saved to csv by sniffLease
+	#using smtplib
+
+	import smtplib
 
 
 sniffLease()
+
